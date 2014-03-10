02d2b4408ec2441971db91c21a442d3bbfcb2a90
Svetlana Filimonova <sfilimonova@me.com>
2014-03-10 21:55:02 +1100

    Removed packman_spec

--- pacman_spec.rb
+++ pacman_spec.rb
@@ -4,7 +4,7 @@ require './pacman'
 
 describe 'next_step' do
   before(:each) do
-  	@pacman = Pacman.new
+    @pacman = Pacman.new
   end
 
   it 'goes nowhere when it is nowhere to go' do
@@ -12,15 +12,15 @@ describe 'next_step' do
   end
   
   it 'goes to the first available empty spot' do
-  	@pacman.next_step([0,0] => :pacman, [0,1] => :empty).must_equal [0,1]
+    @pacman.next_step([0,0] => :pacman, [0,1] => :empty).must_equal [0,1]
   end
   
-  it 'runs away from a pellet' do
-  	@pacman.next_step([0,0] => :empty, [0,1] => :pacman, [0,2] => :pellet).must_equal [0,0]
+  it 'runs away from a blinky' do
+    @pacman.next_step([0,0] => :empty, [0,1] => :pacman, [0,2] => :blinky).must_equal [0,0]
   end
 
   it 'cant jump over positions' do
-  	@pacman.next_step([0,0] => :empty, [0,1] => :pacman, [0,3] => :empty, [0,2] => :empty, [0,4] => :empty).must_equal [0,2]
+    @pacman.next_step([0,0] => :empty, [0,1] => :pacman, [0,3] => :empty, [0,2] => :empty, [0,4] => :empty).must_equal [0,2]
   end
 
 end
